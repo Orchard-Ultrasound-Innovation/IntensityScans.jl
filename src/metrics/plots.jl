6f7068b725e037a5dfecb32bf7188c5e30c8089d
Morten F. Rasmussen <10264458+mofii@users.noreply.github.com>
2021-04-05 14:02:54 -0700

    Updated axis labels in metric plots

--- src/metrics/plots.jl
+++ src/metrics/plots.jl
@@ -2,9 +2,9 @@ using RecipesBase
 using UnitfulRecipes
 
 @recipe function plot(
-    metric::Metric{ISPPA, 1}; 
-    title=type(metric), 
-    label="intensity", 
+    metric::Metric{ISPPA, 1};
+    title=type(metric),
+    label="intensity",
     axes="nil",
     coordinates = nothing,
 )
@@ -14,15 +14,15 @@ using UnitfulRecipes
               ;coordinates=scan1d.coordinates")
     end
     axis = Symbol(axes)
-    xguide --> "Position / m ($(axis) axis)"
+    xguide --> "$(axis)-axis / mm"
     yguide --> "Intensity"
     return string.(get_axis_positions(Val(axis), coordinates)), metric.val
 end
 
 @recipe function plot(
-    metric::Metric{ISPTA, 1}; 
-    title=type(metric), 
-    label="intensity", 
+    metric::Metric{ISPTA, 1};
+    title=type(metric),
+    label="intensity",
     axes="nil",
     coordinates = nothing,
 )
@@ -38,9 +38,9 @@ end
 end
 
 @recipe function plot(
-    metric::Metric{MI, 1}; 
-    title=type(metric), 
-    label="Mechanical Index", 
+    metric::Metric{MI, 1};
+    title=type(metric),
+    label="Mechanical Index",
     axes="nil",
     coordinates = nothing,
 )
@@ -50,128 +50,128 @@ end
               ;coordinates=scan1d.coordinates")
     end
     axis = Symbol(axes)
-    xguide --> "Position / m ($(axis) axis)"
+    xguide --> "$(axis)-axis / mm"
     yguide --> "Mechanical Index"
     return string.(get_axis_positions(Val(axis), coordinates)), metric.val
 end
 
 @recipe function plot(
-    metric::Metric{ISPPA, 2}; 
-    title=type(metric), 
-    axes="nil", 
+    metric::Metric{ISPPA, 2};
+    title=type(metric),
+    axes="nil",
     coordinates = nothing,
 )
     axes == "nil" && error("Keyword missing: axes=\"xy\" or axes=\"yz\"..")
     seriestype := :heatmap
-    xguide --> "Position / m ($(axes[1]) axis)"
-    yguide --> "Position / m ($(axes[2]) axis)"
+    xguide --> "$(axes[1])-axis / mm"
+    yguide --> "$(axes[2])-axis / mm"
 
     return plot_2d_metric(metric.val, coordinates, axes)
 end
 
 @recipe function plot(
-    metric::Metric{ISPTA, 2}; 
-    title=type(metric), 
-    axes="nil", 
+    metric::Metric{ISPTA, 2};
+    title=type(metric),
+    axes="nil",
     coordinates = nothing,
 )
     axes == "nil" && error("Keyword missing: axes=\"xy\" or axes=\"yz\"..")
     seriestype := :heatmap
-    xguide --> "Position / m ($(axes[1]) axis)"
-    yguide --> "Position / m ($(axes[2]) axis)"
+    xguide --> "$(axes[1])-axis / mm"
+    yguide --> "$(axes[2])-axis / mm"
 
     return plot_2d_metric(metric.val, coordinates, axes)
 end
 
 @recipe function plot(
-    metric::Metric{MI, 2}; 
-    title=type(metric), 
-    axes="nil", 
+    metric::Metric{MI, 2};
+    title=type(metric),
+    axes="nil",
     coordinates = nothing,
 )
     axes == "nil" && error("Keyword missing: axes=\"xy\" or axes=\"yz\"..")
     seriestype := :heatmap
-    xguide --> "Axis $(axes[1])"
-    yguide --> "Axis $(axes[2])"
+    xguide --> "$(axes[1])-axis / mm"
+    yguide --> "$(axes[2])-axis / mm"
 
     return plot_2d_metric(metric.val, coordinates, axes)
 end
 
 @recipe function plot(
     metric::Metric{ISPPA, 3};
-    xslice = NaN, 
-    yslice = NaN, 
-    zslice = NaN, 
+    xslice = NaN,
+    yslice = NaN,
+    zslice = NaN,
     coordinates = nothing,
     axes="xyz"
 )
     seriestype := :heatmap
     slice, slice_axis = get_slice(xslice, yslice, zslice)
     data, axes = plot_3d_metric(
-        metric.val, 
+        metric.val,
         slice,
         slice_axis,
         axes,
         coordinates,
     )
     title --> type(metric) * " ($(slice_axis)slice $slice)"
-    xguide --> "Position / m ($(axes[1]) axis)"
-    yguide --> "Position / m ($(axes[2]) axis)"
+    xguide --> "$(axes[1])-axis / mm"
+    yguide --> "$(axes[2])-axis / mm"
     return data
 end
 
 @recipe function plot(
-    metric::Metric{ISPTA, 3}; 
-    xslice = NaN, 
-    yslice = NaN, 
-    zslice = NaN, 
+    metric::Metric{ISPTA, 3};
+    xslice = NaN,
+    yslice = NaN,
+    zslice = NaN,
     coordinates = nothing,
     axes="xyz"
 )
     seriestype := :heatmap
     slice, slice_axis = get_slice(xslice, yslice, zslice)
     data, axes = plot_3d_metric(
-        metric.val, 
+        metric.val,
         slice,
         slice_axis,
         axes,
         coordinates,
     )
     title --> type(metric) * " ($(slice_axis)slice $slice)"
-    xguide --> "Position / m ($(axes[1]) axis)"
-    yguide --> "Position / m ($(axes[2]) axis)"
+    xguide --> "$(axes[1])-axis / mm"
+    yguide --> "$(axes[2])-axis / mm"
     return data
 end
 
 @recipe function plot(
-    metric::Metric{MI, 3}; 
-    xslice = NaN, 
-    yslice = NaN, 
-    zslice = NaN, 
+    metric::Metric{MI, 3};
+    xslice = NaN,
+    yslice = NaN,
+    zslice = NaN,
     coordinates = nothing,
     axes="xyz",
 )
     seriestype := :heatmap
     slice, slice_axis = get_slice(xslice, yslice, zslice)
     data, axes = plot_3d_metric(
-        metric.val, 
+        metric.val,
         slice,
         slice_axis,
         axes,
         coordinates,
     )
     title --> type(metric) * " ($(slice_axis)slice $slice)"
-    xguide --> "Position / m ($(axes[1]) axis)"
-    yguide --> "Position / m ($(axes[2]) axis)"
+    xguide --> "$(axes[1])-axis / mm"
+    yguide --> "$(axes[2])-axis / mm"
     return data
 end
 
 
 
 function plot_3d_metric(
-    data, 
-    slice, 
-    slice_axis, 
+    data,
+    slice,
+    slice_axis,
     axes,
     coordinates,
 )
